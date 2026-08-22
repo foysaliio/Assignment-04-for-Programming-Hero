8a2aee44943060af588228cacc2634687cf0716d
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-22 23:34:45 +0600

    feat(ts): solve table booking confirmation problem

---
+++ 02 - Table Booking Confirmation/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const formatBookingConfirmation = (booking) => {
+    return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`;
+};
+console.log(formatBookingConfirmation({ name: "Aisha", guests: 4, time: "7:00 PM" }));
+console.log(formatBookingConfirmation({ name: "Rahim", guests: 2, time: "8:30 PM" }));
